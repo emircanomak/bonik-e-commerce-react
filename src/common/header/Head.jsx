5be70641f94536151ab1851690ab8a4241520c36
Emircan Omak <omakemircan@hotmail.com>
2022-06-03 01:08:24 +0300

    Bonik e-commerce

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> +90 0546 870 32 84</label>
+            <i className='fa fa-envelope'></i>
+            <label> omakemircan@hotmail.com</label>
+          </div>
+          <div className='right row RText'>
+            <label>Theme FAQ"s</label>
+            <label>Need Help?</label>
+            <span>🏳️‍⚧️</span>
+            <label>EN</label>
+            <span>🏳️‍⚧️</span>
+            <label>USD</label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
